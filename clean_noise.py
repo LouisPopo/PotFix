e3eceb81c53e658202c88afb68258a9f83fb3fdc
Louis Popovic <louis.popovic@polymtl.ca>
2020-01-18 16:59:07 -0500

    ad data from ride

---
+++ clean_noise.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+
+def reduce_noise(filename):
+    df = pd.read_csv(filename, usecols=["time", "az"])
+    #df["az"] = abs(df["az"])
+    df['MA'] = df.rolling(window=5).mean()
+    return df
+
+if __name__  == "__main__":
+    filename = str(sys.argv[1])
+    clean = reduce_noise(filename)
+
+    clean.plot()
+    plt.show()
+    print(clean)
