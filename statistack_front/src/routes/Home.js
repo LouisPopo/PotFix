880142c9cf3732afa9f5f88dabf69e41cf4f4f26
Yannis Tarfa <info@yannistarfa.com>
2020-01-18 15:32:18 -0500

    Basic Home component structure

--- statistack_front/src/routes/Home.js
+++ statistack_front/src/routes/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import Map from '../components/Map';
+
+const HomeContainer = styled.div`
+`;
+
+const NavbarContainer = styled.div`
+    height: 50px;
+    width: 100%;
+`;
+
+const MapContainer = styled.div`
+    width: 100%;
+`;
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <p> Hello, World!</p>
-            </div>
+            <HomeContainer>
+                <NavbarContainer>
+
+                </NavbarContainer>
+
+                <MapContainer>
+                    <Map />
+                </MapContainer>
+            </HomeContainer>
         )
     }
 }
