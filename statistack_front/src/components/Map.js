a98a1aaea8df942144a97c17073ef171bba2d309
Yannis Tarfa <info@yannistarfa.com>
2020-01-18 22:37:44 -0500

    map pulls data from json file

--- statistack_front/src/components/Map.js
+++ statistack_front/src/components/Map.js
@@ -3,23 +3,12 @@ import { compose, withProps, lifecycle } from "recompose";
 import { withScriptjs, withGoogleMap, GoogleMap} from "react-google-maps";
 import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
 
-const heatmapData = [
-    { lat: 37.782, lng: -122.447, weight: 5 },
-    { lat: 37.782, lng: -122.443, weight: 5 },
-    { lat: 37.782, lng: -122.441, weight: 5 },
-    { lat: 37.782, lng: -122.439, weight: 5 },
-    { lat: 37.782, lng: -122.435, weight: 5 },
-    { lat: 37.785, lng: -122.447, weight: 5 },
-    { lat: 37.785, lng: -122.445, weight: 5 },
-    { lat: 37.785, lng: -122.441, weight: 5 },
-    { lat: 37.785, lng: -122.437, weight: 5 },
-    { lat: 37.785, lng: -122.435, weight: 5 }
-];
+import heatmapData from './sample_data.json';
 
 const Map = compose(
     withProps({
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
+        containerElement: <div style={{ height: `700px` }} />,
         mapElement: <div style={{ height: `100%` }} />
     }),
     withScriptjs,
@@ -27,9 +16,7 @@ const Map = compose(
 )(props => 
     <GoogleMap
         defaultZoom={14}
-        // Centre cote des neiges
-        // defaultCenter={{ lat: 45.504136, lng: -73.614522 }}
-        defaultCenter={{ lat: 37.782, lng: -122.447 }}
+        defaultCenter={{ lat: 45.504136, lng: -73.614522 }}
     >
         <HeatmapLayer
             data={props.data.map(function(p) {
@@ -40,7 +27,7 @@ const Map = compose(
             })}
 
             options={{
-                radius: 20,
+                radius: 10,
                 opacity: 0.5,
             }}
         />
@@ -52,9 +39,6 @@ export default class MapContainer extends React.Component {
         return (
             <Map
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZubQYc2srdfMo0lxnZOc0ZgqxgQEy2Cw&v=3.exp&libraries=geometry,drawing,places,visualization"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `700px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
                 data={heatmapData}
             />
         )
