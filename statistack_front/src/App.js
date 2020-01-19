a7c830156a9b415b49df5c58bfc7d15e4c8c63ba
Yannis Tarfa <info@yannistarfa.com>
2020-01-18 22:45:49 -0500

    Added globalstyle

--- statistack_front/src/App.js
+++ statistack_front/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Home from './routes/Home';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 100;
+  }
+`
 
 export default class App extends React.Component {
 
     render() {
         return (
-            <Home />
+            <div>
+                <GlobalStyle />
+                <Home />
+            </div>
         )
     }
 }
